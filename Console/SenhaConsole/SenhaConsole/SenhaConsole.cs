18dd517d5bf468041893a4452315fdf9ee68d057
Cícero Lacerda <matheuslacerda.08@gmail.com>
2020-01-28 15:50:15 -0300

    Primeiro Commit

---
+++ Console/SenhaConsole/SenhaConsole/SenhaConsole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SenhaConsole
+{
+    public class MascararSenhaConsole
+    {
+        public static string LerSenha()
+        {
+            string senha = "";
+            ConsoleKeyInfo console;
+
+            do
+            {
+                console = Console.ReadKey(true);
+                if (console.Key == ConsoleKey.Backspace)
+                {
+                    if (senha.Length > 0)
+                    {
+                        //Pegar a localização do cursor a partir da esquerda
+                        var posiçãoX = Console.CursorLeft;
+                        //Pegar a localização do cursor a partir de cima
+                        var posiçãoY = Console.CursorTop;
+                        //Setar a posição do cursor voltando uma casa para a esquerda
+                        Console.SetCursorPosition(posiçãoX - 1, posiçãoY);
+                        //Limpar a casa com espaço
+                        Console.Write(" ");
+                        //Como colocamos um espaço, precisamos voltar de novo
+                        Console.SetCursorPosition(posiçãoX - 1, posiçãoY);
+                        //Pegar a senha sem o último caracter
+                        senha = senha.Substring(0, senha.Length - 1); 
+                    }
+                }
+                else if (console.Key != ConsoleKey.Enter)
+                {
+                    senha += console.KeyChar;
+                }
+
+            } while (console.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return senha;
+        }
+    }
+}
